9d45e220445cbaf0a579628a90779c610839e670
Musa-The-Best <musaaleghari15@gmail.com>
2026-02-27 14:57:08 +0500

    Add files via upload

---
+++ string operation.py
@@ -0,0 +1,13 @@
+first_name = "GOAT"
+last_name = "BIN Laden"
+full_name = first_name+last_name
+example = "BAA"*5
+print("First Name :", first_name)
+print("Last Name :", last_name)
+print("Full Name :", full_name)
+print("string Multiplied 5 times gives this result :", example)
+word = 'GOAT'
+print("Length of string :", len(word))
+print("First Letter of string :", word[0])
+print("Last Letter of string :", word[4])
+print("string sliced :", word[0:3])
