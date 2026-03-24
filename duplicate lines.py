51b2e1fc0724bf1b8051d802760b5721b8372f8b
Musa-The-Best <musaaleghari15@gmail.com>
2026-03-24 10:40:46 +0500

    Add files via upload

---
+++ duplicate lines.py
@@ -0,0 +1,10 @@
+outputFile = open('UpdadatedFile.txt', 'w')
+inputFile = open('Repeated.rxr', 'r')
+lines_seen_so_far = set()
+print("Eliminating duplicate lines.......")
+for line in inputFile:
+    if line not in lines_seen_so_far:
+        outputFile.write(line)
+        lines_seen_so_far.add(line)
+inputFile.close()
+outputFile.close()
