6f2b19100575267b7bc89b83f332780efad3ff5b
Musa-The-Best <musaaleghari15@gmail.com>
2026-03-26 13:12:38 +0500

    Add files via upload

---
+++ number swapping.py
@@ -0,0 +1,6 @@
+x = int(input("Enter value of X: "))
+y = int(input("Enter value of Y: "))
+z = int(input("Enter value of Z: "))
+numbers = [x, y, z]
+numbers.sort()
+print("Ascending order:", numbers)
